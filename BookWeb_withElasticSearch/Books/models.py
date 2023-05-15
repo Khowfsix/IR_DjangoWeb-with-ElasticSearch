5f1fa50ce059f0aa44d6f83a1c6dcfe56273f9a1
Khowfsix <89312102+Khowfsix@users.noreply.github.com>
2023-05-15 21:47:19 +0700

    config template

--- BookWeb_withElasticSearch/Books/models.py
+++ BookWeb_withElasticSearch/Books/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    Ten = models.CharField(255)
+    MaSanPham = models.CharField(255)
+    Gia = models.FloatField()
+    
