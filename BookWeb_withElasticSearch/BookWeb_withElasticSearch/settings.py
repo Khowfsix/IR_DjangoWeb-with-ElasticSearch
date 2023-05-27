8d7a851a5faaeb5828fdd9ac897f884fc53c754a
Khowfsix <89312102+Khowfsix@users.noreply.github.com>
2023-05-27 17:13:45 +0700

    sometthing

--- BookWeb_withElasticSearch/BookWeb_withElasticSearch/settings.py
+++ BookWeb_withElasticSearch/BookWeb_withElasticSearch/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': 'localhost:9200'
+    },
+}
--- BookWeb_withElasticSearch/Books/views.py
+++ BookWeb_withElasticSearch/Books/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.registries import registry
+from .models import Book
 
 # Create your views here.
 def index(request):
     return render(request, 'BookWeb_withElasticSearch/Books/template/index.html')
 
+
+@registry.register_document
+class BookDocument(Document):
+    class Index:
+        # Name of the Elasticsearch index
+        name = 'books'
+        # See Elasticsearch Indices API reference for available settings
+        settings = {'number_of_shards': 1, 'number_of_replicas': 0}
+
+    class Django:
+        model = Book # The model associated with this Document
+
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            'title',
+            'author',
+            'description',
+        ]
