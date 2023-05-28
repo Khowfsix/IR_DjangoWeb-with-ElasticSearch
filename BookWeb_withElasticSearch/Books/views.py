b7e760fc3283c53096dda3e4806820253dee9c73
Khowfsix <89312102+Khowfsix@users.noreply.github.com>
2023-05-29 04:58:46 +0700

    searching with keyword 1

--- BookWeb_withElasticSearch/Books/views.py
+++ BookWeb_withElasticSearch/Books/views.py
@@ -57,7 +57,7 @@ def index_view(request):
     query = {
         "match_all": {},
     }
-    search_result = search(query, 3000)
+    search_result = search(query, 300)
 
     # Lấy kết quả query sang object Books
     for hit in search_result['hits']['hits']:
@@ -107,25 +107,86 @@ def detail_view(request, book):
                   context=detailContext)
 
 
-def search_view(request, keyword):
+def search_keyword_view(request):
     """
     Hiển thị danh sách các quyển sách có liên quan đến
         từ khóa được tìm kiếm theo thứ tự
     Input: Từ khóa nhập vào thanh Search
     """
+    keyword = request.GET.get('keyword')
+    if (keyword is None):
+        return index_view(request)
 
-    # Thực hiện truy vấn
-    ## Query all
+    listBooks = []
+
+    # Truy vấn dữ liệu
     query = {
-        "match_all": {
+        "bool": {
+            "should": [
+                {
+                    "match": {
+                        "Danh mục": keyword
+                    }
+                },
+                {
+                    "match": {
+                        "Tác giả": keyword
+                    }
+                },
+                {
+                    "match": {
+                        "Tên": keyword
+                    }
+                },
+                {
+                    "match": {
+                        "Dịch giả": keyword
+                    }
+                },
+                {
+                    "match": {
+                        "Nhà xuất bản": keyword
+                    }
+                },
+                {
+                    "match": {
+                        "Giới thiệu sách": keyword
+                    }
+                }
+            ],
+            "minimum_should_match": 1
         }
     }
-    ## Search
-    search_result = search(query=query)
-    hits = search_result["hits"]["hits"]
+    search_result = search(query, 300)
+
+    # Lấy kết quả query sang object Books
+    for hit in search_result['hits']['hits']:
+        JSbook = hit.get('_source')
+        ObBook = Book()
+        ObBook.DanhMuc = JSbook.get('Danh mục')
+        ObBook.LinkImage = JSbook.get('LinkImage')
+        ObBook.Ten = JSbook.get('Tên')
+        ObBook.MaSanPham = JSbook.get('Mã sản phẩm')
+        ObBook.TacGia = JSbook.get('Tác giả')
+        ObBook.NhaXuatBan = JSbook.get('Nhà xuất bản')
+
+        if (JSbook.get('Số trang') is not None):
+            ObBook.SoTrang = int(JSbook.get('Số trang'))
+
+        ObBook.KichThuoc = JSbook.get('Kích thước')
+
+        if (JSbook.get('Ngày phát hành') is not None):
+            ObBook.NgayPhatHanh = datetime.strptime(JSbook.get('Ngày phát hành'), '%d-%m-%Y')
+
+        ObBook.GiaBia = JSbook.get('Giá bìa')
+        ObBook.GiaNhaNam = JSbook.get('Giá Nhã Nam')
+        ObBook.GioiThieuSach = JSbook.get('Giới thiệu sách')
+
+        print(ObBook.Ten)
+        listBooks.append(ObBook)
 
     searchContext = {
-
+        "Books": listBooks
     }
     return render(request=request,
                   template_name='index.html',
