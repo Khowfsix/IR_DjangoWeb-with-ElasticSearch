7259f1b3794118fe8de45ef3acc04393df5b5494
Nguyễn Ngọc Phát <80611167+ngocphatIT@users.noreply.github.com>
2023-06-02 15:32:57 +0700

    add variavle DEV

--- BookWeb_withElasticSearch/Books/views.py
+++ BookWeb_withElasticSearch/Books/views.py
@@ -16,6 +16,13 @@ ELASTIC_PASSWORD = "-JpPuDA43hk+xp*OYAPq"
 # The path of ca certificates
 CA_CERTS = "C:/UTEXLMS/Nam3_2022_2023/HK2/TruyTimThongTin/elasticsearch-8.6.2/config/certs/http_ca.crt"
 
+DEV="NGOCPHAT"
+
+if DEV=="NGOCPHAT":
+    ELASTIC_PASSWORD = "AO_Xpp=erLN89MGxutL7"
+    CA_CERTS="P:/Programming/JAVA/Enviroment/elasticsearch-8.6.2-windows-x86_64/elasticsearch-8.6.2/config/certs/http_ca.crt"
+
+
 # Create the client instance
 es = Elasticsearch(
     hosts=HOST,
